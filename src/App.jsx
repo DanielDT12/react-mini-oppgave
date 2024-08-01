2acca3e1d918075c36319cbef780887398d64838
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-08-01 14:01:39 +0200

    lagt til components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import "./css/App.css";
 
 function App() {
 	return (
 		<>
 			<Header />
+			<Main />
+			<Footer />
 		</>
 	);
 }
