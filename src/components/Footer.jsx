7771f1b6e9d7fc91fc47a287a0db9726d3ecf666
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-08-01 14:01:31 +0200

    lagd footer og main component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+	return (
+		<footer className="primary-footer">
+			<div>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore,
+				voluptates!
+			</div>
+			<div>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, eligendi.
+			</div>
+			<div>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+				soluta!
+			</div>
+		</footer>
+	);
+}
